2add9387358a6b4649426c769ba69f4b567db0e8
Mausmi Sinha <sinhatanu2001@gmail.com>
2023-08-17 19:02:11 +0530

    Quantum Naive Bayes

---
+++ Quantum_circuits.py
@@ -0,0 +1,12 @@
+from qskit import QuantumCircuit, Aer, execute
+from qiskit.visulaisation import plot_histogram
+import matplotlib.pyplot as plt
+
+def run_circuit(qc, simulator='statevector_simulator', shots=1, hist=True):
+    backend = Aer.get_backend(simulator)
+    results = execute(qc, backend, shots=shots).result().get_counts()
+    return plot_histogram(results, figsize=(18,4)) if hist else results
+
+qc = QuantumCircuit(4)
+qc.h([1,2,3,4])
+run_circuit(qc)
